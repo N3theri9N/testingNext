2feb27e2ebdd155a7e894270d4d481aee9e0a754
N3theri9N <nnea5215@gmail.com>
2022-08-25 13:40:55 +0900

    sdktestcommit

---
+++ pages/api/generateJWT.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+
+const userKey = "b453dfe3-2d8a-4528-82aa-8ce52f0568b4";
+const payload = {
+  appId: 'https://testing-next-zeta.vercel.app/', 
+  platform: "web"
+};
+
+const options = {
+  header: { typ: "JWT", alg: "HS256" },
+  expiresIn: 60 * 5 // expire time: 5 mins
+};
+
+function generateJWT(req, res) {
+  try {
+    const clientToken = jwt.sign(payload, userKey, options);
+    res.json({ appId: payload.appId, token: clientToken });
+  } catch (e) {
+    console.log("jwt generate err ", e.name, e.message);
+  }
+}
+
+// generateJWT.js(Server) append
+// export default (req, res) => {
+//   if (req.method === "GET") return generateJWT(req, res);
+//   // if (req.method === "POST") return generateJWT(req, res);
+//  };
+
+export default function handler(req, res){
+  if (req.method === "GET") return generateJWT(req, res);
+  // if (req.method === "POST") return generateJWT(req, res);
+};
+ 
