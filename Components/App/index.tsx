4bd6b5a8ef4803e2530a50431f99c5d938d529cd
N3theri9N <nnea5215@gmail.com>
2022-09-05 18:21:32 +0900

    TS Initialize

---
+++ Components/App/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () : JSX.Element => {
+  return <h2>Hello World!</h2>
+}
+
+export default App;
---
+++ pages/typescript/index.tsx
@@ -0,0 +1,11 @@
+import App from '../../Components/App/index';
+
+const typescript = () : JSX.Element => {
+  return (
+    <>
+      <App />
+    </>
+  )
+}
+
+export default typescript;
