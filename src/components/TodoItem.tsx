daca14f84d764f9bded049a12e44756c92033a57
N3theri9N <nnea5215@gmail.com>
2022-10-10 21:51:41 +0900

    apply style

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,7 @@
+import style from './TodoItem.module.css';
+
 const TodoItem: React.FC<{ text:string }> = (props) => {
-  return <li>{props.text}</li>
+  return <li className={style.item}>{props.text}</li>
 };
 
 export default TodoItem;
